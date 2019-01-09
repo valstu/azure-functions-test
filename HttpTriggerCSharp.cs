8aad94c4aa9b96424d7d0bfe4e4952c46e4488e5
Valtteri Karesto <valtteri.karesto@gmail.com>
2019-01-09 21:56:23 +0200

    Update HttpTriggerCSharp.cs

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -27,7 +27,7 @@ namespace Company.Function
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body!");
         }
     }
 }
